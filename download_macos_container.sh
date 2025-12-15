08c8cb942d3b22b0ff339b2f96273830c10dcc47
anir88 <anir.siliguri@gmail.com>
2025-12-15 21:11:19 +0530

    Added a new code block to check if the container package installed successfully or not

--- download_macos_container.sh
+++ download_macos_container.sh
@@ -5,3 +5,9 @@ echo "Hello $username, Initiating the macos container installation script"
 brew install container
 echo "The container process status is as follows:"
 launchctl list | grep -i container
+export containervar=$(which container)
+if [ -n '$containervar' ];then
+	echo "Macos container is installed at $containervar"
+else
+	echo "Container installation failed"
+fi
