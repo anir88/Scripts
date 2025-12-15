e19b5bfb2495ef6bed757fae33a1fc9cbfd20c2f
anir88 <anir.siliguri@gmail.com>
2025-12-15 20:50:59 +0530

    Added a uer freindly message

--- download_macos_container.sh
+++ download_macos_container.sh
@@ -3,4 +3,5 @@
 export username=$(whoami)
 echo "Hello $username, Initiating the macos container installation script"
 brew install container
+echo "The container process status is as follows:"
 launchctl list | grep -i container
